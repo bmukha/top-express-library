5f365edc4a0b4e5ab0df35f7fe9ae18b5e6861b7
bmukha <47007157+bmukha@users.noreply.github.com>
2021-09-02 22:34:26 +0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ var catalogRouter = require('./routes/catalog');
 var app = express();
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb+srv://bmukha:rlyUAu213F1qJjiy@cluster0.mifkg.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://bmukha:rlyUAu213F1qJjiy@cluster0.mifkg.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
